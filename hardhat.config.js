7a065736fcabab3506b4a07efc47216c43acfedb
Asia Grady <66960776+asiakay@users.noreply.github.com>
2021-04-23 07:34:37 -0400

    added HelloWorld.sol, stored env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
-};
+   solidity: {
+     version: "0.7.4",
+     settings: {
+       optimizer: {
+         enabled: true,
+         runs: 1000
+       }
+     },
+   defaultNetwork: "ropsten",
+   networks: {
+      hardhat: {},
+      ropsten: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+  }
+}
